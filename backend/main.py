1ce24552ecb5d43dd5d6497d9b7d3eedbfdfcbf8
Diego30090 <diego.vaccarini@gmail.com>
2023-01-16 18:56:19 +0100

    backend intScore Working

---
+++ backend/main.py
@@ -0,0 +1,29 @@
+import os
+import json
+from entities import country as c
+
+#File di interesse
+file= 'testIntScore.json'
+
+def move_to_saves(dir):
+    os.chdir(f'{os.getcwd()}/{dir}')
+
+if __name__ == '__main__':
+    c_list = []
+    coun = []
+    move_to_saves('saves')
+    #Apre il file e prende tutti i dati
+    with open(f'{os.getcwd()}/{file}', 'r', encoding='utf8') as f:
+        data= json.load(f)
+    #Prende i tag delle varie nazioni
+    countries_data = data['countries']
+    #Definisce la lista dei tag delle varie nazioni
+    for i in data['countries']:
+        c_list.append(i)
+    #Crea tutte le nazioni presenti in gioco, includendo tutte le variabili di sorta
+    for i in range(len(c_list)):
+        coun.append(c.country(c_list[i], countries_data[c_list[i]]))
+
+    for i in range(len(c_list)):
+        if coun[i].check_intScore() != None:
+            print(f'Nazione: {coun[i].tag}, IntScore: {coun[i].check_intScore()}')
