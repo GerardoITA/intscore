ad2cc7fa21c096605e3240b37c4025a596320b5a
GerardoITA <gerardo.giusti@tiscali.it>
2023-01-16 17:17:13 +0100

    finito setup frontend, creato json statico da usare come base per frontend

---
+++ frontend/src/components/IntscoreHeader.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+  name: "",
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<template>
+  <h1>hello world</h1>
+</template>
+
+<style lang="scss" scoped>
+@use '../style/variables.scss' as *;
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+h1 {
+  color: $BlazeOrange;
+}
+</style>
---
+++ frontend/src/components/TEMPLATE.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+  name: "",
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<template>
+
+</template>
+
+<style lang="scss" scoped>
+@use '../style/variables.scss' as *;
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+</style>
